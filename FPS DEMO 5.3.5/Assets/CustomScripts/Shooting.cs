4c0da5dbf65376eed0749818a6026a603b032167
tshi206 <tshi206@aucklanduni.ac.nz>
2017-10-01 18:58:30 +1300

    add gun animation and sound

---
+++ FPS DEMO 5.3.5/Assets/CustomScripts/Shooting.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour
+{
+
+	private AudioSource _audioSource;
+	private Animation _animation;
+
+	private void Start()
+	{
+		_audioSource = GetComponent<AudioSource>();
+		_animation = GetComponent<Animation>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+		{
+			_audioSource.Play();
+			_animation.Play("GunShot");
+		}
+	}
+}
