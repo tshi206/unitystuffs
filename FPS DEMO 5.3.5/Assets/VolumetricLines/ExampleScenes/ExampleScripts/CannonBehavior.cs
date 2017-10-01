6752ab03d168498686cc4365f92a01611eda9480
tshi206 <tshi206@aucklanduni.ac.nz>
2017-10-02 00:15:21 +1300

    added a simple turret

--- FPS DEMO 5.3.5/Assets/VolumetricLines/ExampleScenes/ExampleScripts/CannonBehavior.cs
+++ FPS DEMO 5.3.5/Assets/VolumetricLines/ExampleScenes/ExampleScripts/CannonBehavior.cs
@@ -7,33 +7,44 @@ public class CannonBehavior : MonoBehaviour {
 	public Transform m_muzzle;
 	public GameObject m_shotPrefab;
 	public Texture2D m_guiTexture;
+	
+	private WaitForSeconds shotDuration = new WaitForSeconds(5f);
+
+	private float nextFire;
 
 	// Use this for initialization
 	void Start () 
 	{
-	
+		nextFire = Time.time;
 	}
 	
 	// Update is called once per frame
-	void Update () 
+	void Update ()
 	{
 		if (Input.GetKey(KeyCode.LeftArrow))
 		{
-			m_cannonRot.transform.Rotate(Vector3.up, -Time.deltaTime * 100f);
+			//m_cannonRot.transform.Rotate(Vector3.up, -Time.deltaTime * 100f);
 		}
 		if (Input.GetKey(KeyCode.RightArrow))
 		{
-			m_cannonRot.transform.Rotate(Vector3.up, Time.deltaTime * 100f);
+			//m_cannonRot.transform.Rotate(Vector3.up, Time.deltaTime * 100f);
 		}
-		if (Input.GetKeyDown(KeyCode.Space))
+		m_cannonRot.transform.Rotate(Vector3.up, Time.deltaTime * 100f);
+		//if (Input.GetKeyDown(KeyCode.Space))
+		//{
+			//GameObject go = GameObject.Instantiate(m_shotPrefab, m_muzzle.position, m_muzzle.rotation) as GameObject;
+			//GameObject.Destroy(go, 3f);
+		//}
+		if (Time.time >= nextFire)
 		{
 			GameObject go = GameObject.Instantiate(m_shotPrefab, m_muzzle.position, m_muzzle.rotation) as GameObject;
 			GameObject.Destroy(go, 3f);
+			nextFire = Time.time + 1f;
 		}
 	}
 
 	void OnGUI()
 	{
-		GUI.DrawTexture(new Rect(0f, 0f, m_guiTexture.width / 2, m_guiTexture.height / 2), m_guiTexture);
+		//GUI.DrawTexture(new Rect(0f, 0f, m_guiTexture.width / 2, m_guiTexture.height / 2), m_guiTexture);
 	}
 }
